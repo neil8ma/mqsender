6430246c884caadad7aa65ef8bcec471b68f5d3d
maming <maming@potevio.com>
2022-08-10 17:21:56 +0800

    入库

---
+++ src/main/java/com/cpit/mqsender/MqsenderApplication.java
@@ -0,0 +1,13 @@
+package com.cpit.mqsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqsenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqsenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpit/mqsender/MqsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpit.mqsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqsenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
